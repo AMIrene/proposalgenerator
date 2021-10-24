8c9cb5edcc4637d1ada257613a67ccb44299edc6
AMIrene <f.adammanik@gmail.com>
2021-10-24 15:05:00 +1100

    queries client file projects added

--- client/src/gql/queries.js
+++ client/src/gql/queries.js
@@ -4,6 +4,42 @@ export const QUERY_USER = gql`
   {
     user {
       email
+      projects {
+        _id
+        projectRef
+        projectTitle
+        projectManager
+        createdAt
+      }
     }
   }
+`;
+
+//get all projects
+
+export const QUERY_PROJECTS = gql`
+query getProjects {
+  projects {
+    _id
+    projectRef
+    projectTitle
+    projectManager
+    createdAt
+  }
+}
+`;
+
+// get a single project
+export const QUERY_SINGLE_PROJECT = gql`
+query getSingleProject($projectId: ID!) {
+  project(projectId: $$projectId) {
+    _id
+    projectRef
+    projectTitle
+    projectManager
+    createdAt
+  }
+}
+
+
 `;
