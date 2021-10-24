7a81a06d1e870c5bbd90e3c26881cca91852a5de
AMIrene <f.adammanik@gmail.com>
2021-10-24 15:04:18 +1100

    edited seeds.js file to incl. user & project seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,20 +1,52 @@
 const db = require('./connection');
-const { User } = require('../models');
+const { User, Project } = require('../models');
+
+const userSeeds = require('./userSeeds.json');
+const projectSeeds = require('./projectSeeds.json');
 
 db.once('open', async () => {
-  await User.deleteMany();
+  try {
+    await Project.deleteMany({});
+    await User.deleteMany({});
 
-  await User.create({
-    email: 'pamela@testmail.com',
-    password: 'password12345'
-  });
+    await User.create(userSeeds);
 
-  await User.create({
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
+    for (let i = 0; i < projectSeeds.length; i++) {
+      const { _id, projectManager } = await Project.create(projectSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: projectManager },
+        {
+          $addToSet: {
+            projects: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-  console.log('users seeded');
-
-  process.exit();
+  console.log('all done!');
+  process.exit(0);
 });
+
+
+
+// db.once('open', async () => {
+//   await User.deleteMany();
+
+//   await User.create({
+//     email: 'pamela@testmail.com',
+//     password: 'password12345'
+//   });
+
+//   await User.create({
+//     email: 'eholt@testmail.com',
+//     password: 'password12345'
+//   });
+
+//   console.log('users seeded');
+
+//   process.exit();
+// });
