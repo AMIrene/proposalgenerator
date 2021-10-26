e8d0bcb24cb4b2c0c4779afedcf44233a0341a01
AMIrene <f.adammanik@gmail.com>
2021-10-26 21:22:03 +1100

    added project page template

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "../components/Container";
+import { H2 } from '../components/Text';
+
+const Projects = () => {
+  return (
+    <Container alignContent="center">
+      <H2>Project List</H2>
+    </Container>
+  );
+};
+
+export default Projects;
