a15900ecee85617fd4ce9e5053f3ebdf3e0ad80d
AMIrene <f.adammanik@gmail.com>
2021-10-30 17:45:37 +1100

    search input

--- client/src/pages/Projects1.js
+++ client/src/pages/Projects1.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import ProjectList from '../components/ProjectList';
-
-import { QUERY_PROJECTS } from '../gql/queries';
+import { SEARCH_PROJECTS } from '../gql/queries';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const Projects1 = () => {
-    const { loading, error, data } = useQuery(QUERY_PROJECTS);
+
+    const [searchText, setSearchText] = useState("Design");
+    
+    console.log(searchText);
+    
+    const { loading, error, data } = useQuery(SEARCH_PROJECTS, {
+        variables: { searchkey: searchText }
+    });
+
+    console.log(data);
+
     if (loading) {
-        return <p>Loading...</p>;
+        return <p>Loading....</p>;
     }
     if (error) return <p>Error :(</p>;
-    
+
     return (
-        
-        <ProjectList
-            projects={data.projects}
-                          
-        />
+        <div>
+            <Box>
+                <TextField id="outlined-basic" value={searchText} variant="outlined" onChange={(event) => {setSearchText(event.target.value)}} />
+                <Button variant="contained" onClick={()=>{}}>Search</Button>
+            </Box>
+            <ProjectList
+                projects={data.searchProject}
+            />
+        </div>
     );
 };
 
 export default Projects1;
+
+//
+// Parent page Projects
+//     - filtertext variable
+//     - searchbar - renders always
+//         - has a callback to set the text
+//     - projectsList - has a default render mode
+//         - also takes in filtertext
+
+
